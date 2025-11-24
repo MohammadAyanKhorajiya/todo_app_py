4e526824c1ef4760a91f336e46ae9895742271fc
Mohammad Ayan <mohammadayankhorajiya@gmial.com>
2025-11-24 11:04:49 +0530

    Add a brief description of your changes

---
+++ todoproject/todo/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def index(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+            return redirect('index')
+    
+    return render(request, 'todo/index.html', {'tasks': tasks})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('index')
